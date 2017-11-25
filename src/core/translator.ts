0452b36e5c52ebcb2c96a37a5d6b8897882f3d22
WittBulter <nanazuimeng123@gmail.com>
2017-11-26 01:09:19 +0800

    Core: improve card styles

--- src/core/translator.ts
+++ src/core/translator.ts
@@ -1,19 +1,26 @@
 import { $fetch, queryParse } from '../utils/http'
 import { API, DEFAULT_QUERYS, ErrorTips } from '../utils/constants'
+import { TranslatorResult } from '../typings/touch-dog'
 
-type BaiduResult = {
-  trans_result: any,
+const findParts = (result: TranslatorResult) => {
+  if (!result.dict_result) return []
+  const means = result.dict_result.simple_means
+  if (!means || !means.symbols || !means.symbols.length) return []
+  const parts = means.symbols[0].parts
+  if (!parts || !parts.length) return []
+  return parts
 }
 export const toEnglish = async(text: string) => {
   try {
-    const result: BaiduResult = await $fetch<BaiduResult>(API.BAIDU, {
+    const result: TranslatorResult = await $fetch<TranslatorResult>(API.BAIDU, {
       method: 'post',
       body: queryParse(DEFAULT_QUERYS.BAIDU, { query: text }),
     })
     if (!result || !result.trans_result) return ErrorTips.translationError
     const data: any = result.trans_result.data
     if (!data || !data.length) return ErrorTips.translationInterruption
-    return (data[0] || {}).dst
+    
+    return { text: (data[0] || {}).dst, parts: findParts(result) }
   } catch (e) {
     console.log(`Translation Error: ${e}`)
     return ErrorTips.translationError
--- src/pool/card.ts
+++ src/pool/card.ts
@@ -1,10 +1,27 @@
 import { EventHub } from './event'
 import { LOGO, STYLES } from '../utils/constants'
+import { TranslatorEvent } from '../typings/touch-dog'
 
 export class Card {
   
   private eventHub: EventHub
   private el: HTMLElement
+  
+  static makeTemplate(trans?: TranslatorEvent): string {
+    if (!trans) return '<p></p>'
+    const title: string = `<p class="-touch-dog-title">${trans.text}</p>`
+    const parts = trans.parts || []
+    const list: string = parts.map(part =>
+      `<li><b>${part.part}</b>${part.means.map((t) => `<span>${t}</span>`).join(';')}</li>`)
+      .join('')
+    const ul: string = list ? `<ul class="-touch-dog-list">${list}</ul>` : ''
+    return `
+      ${title}
+      ${ul}
+      <div class="-touch-dog-logo">${LOGO}</div>
+      <div class="paper"></div>
+    `
+  }
   
   constructor(eventHub: EventHub) {
     this.eventHub = eventHub
@@ -15,12 +32,13 @@ export class Card {
     this.el = this.makeTemplate()
     document.body.appendChild(this.el)
     document.head.appendChild(this.makeStyles())
-    this.eventHub.listen('showCard', (e, result) => {
-      this.update(result)
+    this.eventHub.listen('showCard', (e, trans) => {
+      this.update(trans)
     })
     this.el.addEventListener('click', (e: Event) => {
       e.stopPropagation()
     })
+    this.el.addEventListener('mouseup', (e: Event) => e.stopPropagation())
   }
   
   private makeTemplate(): HTMLElement {
@@ -29,13 +47,7 @@ export class Card {
     _div.style.left = '-2000px'
     _div.setAttribute('id', '-touch-dog')
     _div.classList.add('-touch-dog')
-    _div.innerHTML = `
-      <p>default</p>
-      <ul>
-        <li></li>
-      </ul>
-      <div class="-touch-dog-logo">${LOGO}</div>
-    `
+    _div.innerHTML = Card.makeTemplate()
     return _div
   }
   
@@ -46,9 +58,9 @@ export class Card {
     return styles
   }
   
-  private update(message: any): void {
+  private update(trans: TranslatorEvent): void {
     const SAFE_MARGIN: number = 30
-    const position: any = message.position
+    const position: any = trans.position
     let nextX: number = position.x + 15
     let nextY: number = position.y + 25
     const { height, width } = this.el.getClientRects()[0]
@@ -59,6 +71,7 @@ export class Card {
     if (nextY + height + SAFE_MARGIN > document.documentElement.clientHeight) {
       nextY = nextY - height - SAFE_MARGIN - 25
     }
+    this.el.innerHTML = Card.makeTemplate(trans)
     this.el.style.display = 'none'
     this.el.style.top = `${nextY}px`
     this.el.style.left = `${nextX}px`
